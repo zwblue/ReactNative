fd051c064b7581915e795b8e860e8b2d767cd4e5
zw <18755489161@163.com>
2018-08-26 11:58:36 +0800

    rn项目搭建，注意react-native要0.55.4版本 react-native init myPorject1 --version 0.55.4

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: '欢迎来到安卓世界,\n' +
+    '多姿多彩',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          安卓
+        </Text>
+        <Text style={styles.instructions}>
+          blue RN开发起步
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+          <Text>
+            1111
+          </Text>  
+        </Text>
+        <Text>
+          开始了，起步
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
